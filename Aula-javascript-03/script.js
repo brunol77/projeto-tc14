7ca83a99997b9a320b4a1cf174dd393babe37300
Bruno <brunol77@gmail.com>
2024-02-07 21:13:07 -0300

    populando elementos

---
+++ Aula-javascript-03/script.js
@@ -0,0 +1,64 @@
+const arrayPostagens = [
+    {
+      titulo: "Pop Vegan",
+      subtitulo: "Comida vegana para todos!",
+      data: "06/07/2022",
+      texto: "Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)"
+    },
+    {
+      titulo: "Make Hommus. Not War",
+      subtitulo: "Só delivery, para curtir em casa!",
+      data: "18/08/2022",
+      texto: "Neste restaurante não só pode, como é encorajado comer o antepasto como prato principal. Recomendamos os kibes e a kafta bonina."
+    },
+    {
+      titulo: "Churrascada do Mar",
+      subtitulo: "Melhor do que estar na praia!",
+      data: "30/08/2022",
+      texto: "Todos conhecemos e amamos um bom churrasco, mas o que você acha de experimentar um churrasco focado em frutos do mar? Nós gostamos, experimente e nos conte o que você achou!"
+    },
+  ]
+
+for (let i = 0; i < arrayPostagens.length; i++){
+// Criando Elemento
+const article = document.createElement('article')
+
+// Populando o Elemento
+article.innerHTML = `
+<h3>${arrayPostagens[i].titulo}</h3>
+<p class="subtitulo">${arrayPostagens[i].subtitulo}</p>
+<div class="data">${arrayPostagens[i].data}</div>
+<p>${arrayPostagens[i].texto}</p>`
+
+/// Adicionando no DOM
+const main = document.querySelector('main')
+main.appendChild(article)
+}
+
+
+/// Comentando o código para usar o FOR
+
+/// Criando Elemento
+//const article = document.createElement('article')
+
+/// Populando o Elemento - CODIGO H3 DO HTML
+//article.innerHTML = `<h3>Pop Vegan</h3>
+//<p class="subtitulo">Comida vegana para todos!</p>
+//<div class="data">06/07/2022</div>
+//<p>Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)</p>`
+
+/// Populando o Elemento - PELO ARRAY - 0, 1 OU 2
+//article.innerHTML = `
+//<h3>${arrayPostagens[1].titulo}</h3>
+//<p class="subtitulo">${arrayPostagens[1].subtitulo}</p>
+//<div class="data">${arrayPostagens[1].data}</div>
+//<p>${arrayPostagens[1].texto}</p>`
+
+/// Adicionando no DOM
+//const main = document.querySelector('main')
+//main.appendChild(article)
+
+
+
+
+
